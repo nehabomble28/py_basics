a3d098609d97f8b45f8d5e82bc3fe6c08de76cbd
Neha Bomble <60164727+pandalicious28@users.noreply.github.com>
2020-05-30 20:33:38 +0530

    Accept and return a string using function.

--- strfun.py
+++ strfun.py
@@ -5,8 +5,6 @@ Created on Sat May 30 08:34:02 2020
 
 @author: nehab28
 """
-
-#Session 1 Assignment Day 2 i.e 29/05/2020
 #Write a function named "strfun" that takes a string as input, and simply returns that string.
 
 def strfun(input_str):
