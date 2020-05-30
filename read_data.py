99acc12ebf90521a26921589e24447d66f612da3
Neha Bomble <60164727+pandalicious28@users.noreply.github.com>
2020-05-30 20:31:34 +0530

    Read data from aa file and display.

--- read_data.py
+++ read_data.py
@@ -6,18 +6,7 @@ Created on Sat May 30 15:32:29 2020
 @author: nehab28
 """
 
-#Session 1  Assignment 29/05/2020 Day2
 #Write a python program to read first 10 characters from text file.
-
-'''infile = open('data.txt' , 'r')
-
-lines = ''
-
-for line in infile:
-    lines = line
-infile.close()
-
-print(lines[0:10])'''
 
 with open('data.txt') as f:
     while True:
