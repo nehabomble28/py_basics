bf5a417c5aa3421081cef8b03a5c7959f8c369e6
Neha Bomble <60164727+pandalicious28@users.noreply.github.com>
2020-05-30 20:32:58 +0530

    Read data from a file and display.

--- read_data.py
+++ read_data.py
@@ -5,7 +5,6 @@ Created on Sat May 30 15:32:29 2020
 
 @author: nehab28
 """
-
 #Write a python program to read first 10 characters from text file.
 
 with open('data.txt') as f:
@@ -16,3 +15,4 @@ with open('data.txt') as f:
             break
         print("", c)
         break
+        
