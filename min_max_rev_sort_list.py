15259800bb8ed77523b4709b80f8b394103ec0b3
Neha Bomble <60164727+pandalicious28@users.noreply.github.com>
2020-05-30 20:27:03 +0530

    Find min, max, sorted list and reversed list.

--- min_max_rev_sort_list.py
+++ min_max_rev_sort_list.py
@@ -13,16 +13,13 @@ num = 5
 
 userList = list(int(n) for n in input("Enter the list numbers or elements separated byy space: ").strip().split())[:num]
 print("User List is ", userList)
-
 print("Minimum number in the list is ", min(userList))
 print("Maximum number in the list is ", max(userList))
 
 sortList = userList
 revList = userList
-
 revList.reverse()
 print("Reversed List: ", revList)
-
 sortList.sort()
 print("Sorted UserList: ", sortList)
 
